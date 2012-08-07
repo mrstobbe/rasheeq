3483fa3567331d3c8c784bf833810a9c8c4aaf80
mr.stobbe <mr.stobbe@gmail.com>
2012-08-07 04:54:18 +0000

    Poller now supports basic add/remove handle functionality

--- src/include/Rasheeq/Poller.h
+++ src/include/Rasheeq/Poller.h
@@ -2,20 +2,50 @@
 #define Rasheeq_Poller_H_
 
 #ifdef __cplusplus
+
+#include <functional>
+#include <unordered_map>
+
 namespace R {
 
 class Poller {
+	public:
+		enum Events {
+			peRead = 0x01,
+			peWrite = 0x02,
+			peError = 0x04
+		}; //enum Events
+		typedef std::function<bool(int, void* userArg)> ReadReady;
+		typedef std::function<bool(int, void* userArg)> WriteReady;
+		typedef std::function<void(int, void* userArg)> ErrorOccurred;
+	protected:
+		struct Entry {
+			int fd;
+			int events;
+			ReadReady onRead;
+			WriteReady onWrite;
+			ErrorOccurred onError;
+			void* userArg;
+		}; //struct Entry
 	private:
 		#if RASHEEQ_HAVE_EPOLL
 		int efd_;
 		#endif
+		std::unordered_map<int, Entry> entries_;
 		long timeout_;
 	public:
 		Poller();
 		~Poller();
 	public:
+		bool add(int fd, ReadReady onReadReady, WriteReady onWriteReady);
+		bool add(int fd, ReadReady onReadReady, WriteReady onWriteReady, ErrorOccurred onError);
+		bool add(int fd, ReadReady onReadReady, WriteReady onWriteReady, void* userArg);
+		bool add(int fd, ReadReady onReadReady, WriteReady onWriteReady, ErrorOccurred onError, void* userArg);
 		inline void poll() { this->poll(this->timeout_); };
 		void poll(const long timeout);
+		bool remove(int fd);
+	private:
+		bool add(int fd, ReadReady& onReadReady, WriteReady& onWriteReady, ErrorOccurred* onError, void* userArg);
 }; //class Poller
 
 }; //ns R
--- src/lib/Poller.cpp
+++ src/lib/Poller.cpp
@@ -1,21 +1,104 @@
 #include "Rasheeq/Poller.h"
 
+#include <unistd.h>
+
+#if RASHEEQ_HAVE_EPOLL
+	#include <sys/epoll.h>
+#endif /* RASHEEQ_HAVE_EPOLL */
+
 R::Poller::Poller():
 	#if RASHEEQ_HAVE_EPOLL
 	efd_(-1),
-	#endif
+	#endif /* RASHEEQ_HAVE_EPOLL */
+	entries_(),
 	timeout_(0)
 {
-	#warning Poller::Poller() stubbed
+	#if RASHEEQ_HAVE_EPOLL
+	//#TODO: Test for errors
+	this->efd = ::epoll_create1(0);
+	#endif /* RASHEEQ_HAVE_EPOLL */
 };
 
 R::Poller::~Poller() {
-	#warning Poller::~Poller() stubbed
+	for (auto it = this->entries_.begin(); it != this->entries_.end(); ++it) {
+		//#TODO: Support "close" event trigger
+		::close(it->first);
+	}
+	#if RASHEEQ_HAVE_EPOLL
+	::close(this->efd);
+	this->efd = -1;
+	#endif /* RASHEEQ_HAVE_EPOLL */
+};
+
+bool R::Poller::add(int fd, ReadReady onReadReady, WriteReady onWriteReady) {
+	//#TODO: Sanity check -1
+	return this->add(fd, onReadReady, onWriteReady, NULL, NULL);
+};
+
+bool R::Poller::add(int fd, ReadReady onReadReady, WriteReady onWriteReady, ErrorOccurred onError) {
+	//#TODO: Sanity check -1
+	return this->add(fd, onReadReady, onWriteReady, &onError, NULL);
+};
+
+bool R::Poller::add(int fd, ReadReady onReadReady, WriteReady onWriteReady, void* userArg) {
+	//#TODO: Sanity check -1
+	return this->add(fd, onReadReady, onWriteReady, NULL, userArg);
+};
+
+bool R::Poller::add(int fd, ReadReady onReadReady, WriteReady onWriteReady, ErrorOccurred onError, void* userArg) {
+	//#TODO: Sanity check -1
+	return this->add(fd, onReadReady, onWriteReady, &onError, userArg);
 };
 
 void R::Poller::poll(const long timeout) {
 	#warning Poller::poll() stubbed
 };
+
+bool R::Poller::remove(int fd) {
+	auto it = this->entries_.find(fd);
+	if (it == this->entries_.end())
+		return false;
+	//#TODO: Support "close" event trigger
+	this->entries_.erase(it);
+	::close(fd);
+	return true;
+};
+
+
+bool R::Poller::add(int fd, ReadReady& onReadReady, WriteReady& onWriteReady, ErrorOccurred* onError, void* userArg) {
+	int events = peRead | peWrite;
+	if (onError != NULL)
+		events |= peError;
+	auto it = this->entries_.find(fd);
+	if (it != this->entries_.end()) {
+		Entry& entry = it->second;
+		if (events == entry.events)
+			return false;
+		//#TODO: Support modification (via Entry mod and epoll_ctl as needed)
+		#warning Poller::add() -- Modification of pre-existing poller entry not yet supported
+		return true;
+	}
+	#if RASHEEQ_HAVE_EPOLL
+	uint32_t ep_events = EPOLLET | EPOLLIN | EPOLLOUT;
+	if (onError != NULL)
+		ep_events |= EPOLLET;
+	::epoll_event ep_event = {
+			events: ep_events,
+			data: { fd: fd }
+		};
+	//#TODO: Test for errors
+	::epoll_ctl(this->efd, EPOLL_CTL_ADD, entry.fd, &ep_event);
+	#endif /* RASHEEQ_HAVE_EPOLL */
+	Entry& entry = this->entries_[fd];
+	entry.fd = fd;
+	entry.events = events;
+	entry.onRead = onReadReady;
+	entry.onWrite = onWriteReady;
+	entry.onError = (onError != NULL) ? *onError : ErrorOccurred();
+	entry.userArg = userArg;
+	return true;
+};
+
 
 extern "C" {
 
