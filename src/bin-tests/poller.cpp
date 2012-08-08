0c68b6a621c71baa70636935dda0867d272ba432
mr.stobbe <mr.stobbe@gmail.com>
2012-08-08 03:51:59 +0000

    Fully functional (but extremely basic) poller test

--- src/bin-tests/poller.cpp
+++ src/bin-tests/poller.cpp
@@ -5,11 +5,89 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <cerrno>
+#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
+static R::Poller poller;
+
+static bool writeReady(int sock, void* userArg);
+static bool readReady(int sock, void* userArg);
+
+static bool readReady(int sock, void* userArg) {
+	cout << "readReady(" << sock << ")" << endl;
+	int& listener = *reinterpret_cast<int*>(userArg);
+	if (sock == listener) {
+		sockaddr_in paddr;
+		socklen_t paddr_len = sizeof(paddr);
+		int cli = accept(listener, reinterpret_cast<sockaddr*>(&paddr), &paddr_len);
+		cout << inet_ntoa(paddr.sin_addr) << " connected." << endl;
+		int v = fcntl(cli, F_GETFL, 0);
+		fcntl(cli, F_SETFL, v | O_NONBLOCK);
+		poller.add(cli, readReady, writeReady, userArg);
+	} else {
+		char buf[512];
+		int res = recv(sock, buf, 512, 0);
+		//Do something useful with the data here
+		if ((res == -1) && ((errno == EWOULDBLOCK) || (errno == EAGAIN)))
+			return false;
+		if (res <= 0) {
+			sockaddr_in paddr;
+			socklen_t paddr_len = sizeof(paddr);
+			getpeername(sock, reinterpret_cast<sockaddr*>(&paddr), &paddr_len);
+			cout << inet_ntoa(paddr.sin_addr) << " disconnected." << endl;
+			poller.remove(sock);
+		}
+	}
+	return true;
+};
+
+static bool writeReady(int sock, void* userArg) {
+	cout << "writeReady(" << sock << ")" << endl;
+	return true;
+};
 
 int main(int argc, char** argv) {
-	int sock = ::socket(AF_INET, SOCK_STREAM, 0);
-	
+	int listener = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listener == -1) {
+		perror("socket()");
+		return 1;
+	}
+	int v = fcntl(listener, F_GETFL, 0);
+	if (v == -1) {
+		perror("fcntl(get)");
+		return 1;
+	}
+	if (fcntl(listener, F_SETFL, v | O_NONBLOCK) == -1) {
+		perror("fcntl(set)");
+		return 1;
+	}
+	sockaddr_in addr = {
+		sin_family: AF_INET,
+		sin_port: htons(12345),
+		sin_addr: { s_addr: 0 }
+	};
+	v = 1;
+	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &v, sizeof(v)) == -1) {
+		perror("setsockopt()");
+		return 1;
+	}
+	if (bind(listener, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) == -1) {
+		perror("bind()");
+		return 1;
+	}
+	if (listen(listener, SOMAXCONN) == -1) {
+		perror("listen()");
+		return 1;
+	}
+	poller.add(listener, readReady, writeReady, &listener);
+	while (true) {
+		poller.poll();
+	}
+	return 0;
 };
