9146843a4b78a25953fe5692946a6106d0fb27a6
mr.stobbe <mr.stobbe@gmail.com>
2012-08-08 02:40:26 +0000

    Added actual poller code for epoll

--- src/include/Rasheeq/Poller.h
+++ src/include/Rasheeq/Poller.h
@@ -5,23 +5,27 @@
 
 #include <functional>
 #include <unordered_map>
+#include <unordered_set>
 
 namespace R {
 
 class Poller {
+	public:
+		static int maxPerPoll;
 	public:
 		enum Events {
 			peRead = 0x01,
 			peWrite = 0x02,
 			peError = 0x04
 		}; //enum Events
-		typedef std::function<bool(int, void* userArg)> ReadReady;
-		typedef std::function<bool(int, void* userArg)> WriteReady;
-		typedef std::function<void(int, void* userArg)> ErrorOccurred;
+		typedef std::function<bool(int, void*)> ReadReady;
+		typedef std::function<bool(int, void*)> WriteReady;
+		typedef std::function<void(int, void*)> ErrorOccurred;
 	protected:
 		struct Entry {
 			int fd;
 			int events;
+			int activeEvents;
 			ReadReady onRead;
 			WriteReady onWrite;
 			ErrorOccurred onError;
@@ -32,7 +36,8 @@ class Poller {
 		int efd_;
 		#endif
 		std::unordered_map<int, Entry> entries_;
-		long timeout_;
+		std::unordered_set<int> haveEvent_;
+		int timeout_;
 	public:
 		Poller();
 		~Poller();
@@ -42,7 +47,7 @@ class Poller {
 		bool add(int fd, ReadReady onReadReady, WriteReady onWriteReady, void* userArg);
 		bool add(int fd, ReadReady onReadReady, WriteReady onWriteReady, ErrorOccurred onError, void* userArg);
 		inline void poll() { this->poll(this->timeout_); };
-		void poll(const long timeout);
+		void poll(const int timeout);
 		bool remove(int fd);
 	private:
 		bool add(int fd, ReadReady& onReadReady, WriteReady& onWriteReady, ErrorOccurred* onError, void* userArg);
@@ -63,7 +68,7 @@ int rasheeq_poller_add(rasheeq_poller_t* poller, int fd, rasheeq_readready_callb
 rasheeq_poller_t* poller_create();
 void poller_destroy(rasheeq_poller_t* poller);
 void rasheeq_poller_poll(rasheeq_poller_t* poller);
-void rasheeq_poller_timed_poll(rasheeq_poller_t* poller, const long timeout);
+void rasheeq_poller_timed_poll(rasheeq_poller_t* poller, const int timeout);
 int rasheeq_poller_remove(rasheeq_poller_t* poller, int fd);
 
 #ifdef __cplusplus
--- src/lib/Poller.cpp
+++ src/lib/Poller.cpp
@@ -1,21 +1,25 @@
 #include "Rasheeq/Poller.h"
 
+#include <cerrno>
 #include <unistd.h>
 
 #if RASHEEQ_HAVE_EPOLL
 	#include <sys/epoll.h>
 #endif /* RASHEEQ_HAVE_EPOLL */
 
+int R::Poller::maxPerPoll = 64;
+
 R::Poller::Poller():
 	#if RASHEEQ_HAVE_EPOLL
-	efd_(-1),
+		efd_(-1),
 	#endif /* RASHEEQ_HAVE_EPOLL */
 	entries_(),
+	haveEvent_(),
 	timeout_(0)
 {
 	#if RASHEEQ_HAVE_EPOLL
-	//#TODO: Test for errors
-	this->efd_ = ::epoll_create1(0);
+		//#TODO: Test for errors
+		this->efd_ = ::epoll_create1(0);
 	#endif /* RASHEEQ_HAVE_EPOLL */
 };
 
@@ -25,8 +29,8 @@ R::Poller::~Poller() {
 		::close(it->first);
 	}
 	#if RASHEEQ_HAVE_EPOLL
-	::close(this->efd_);
-	this->efd_ = -1;
+		::close(this->efd_);
+		this->efd_ = -1;
 	#endif /* RASHEEQ_HAVE_EPOLL */
 };
 
@@ -50,14 +54,58 @@ bool R::Poller::add(int fd, ReadReady onReadReady, WriteReady onWriteReady, Erro
 	return this->add(fd, onReadReady, onWriteReady, &onError, userArg);
 };
 
-void R::Poller::poll(const long timeout) {
-	#warning Poller::poll() stubbed
+void R::Poller::poll(const int timeout) {
+	for (auto it = this->haveEvent_.begin(); it != this->haveEvent_.end(); ++it) {
+		Entry& entry = this->entries_[*it];
+		if (((entry.activeEvents & peWrite) != 0) && (entry.onWrite(entry.fd, entry.userArg)))
+			entry.activeEvents ^= peWrite;
+		if (((entry.activeEvents & peRead) != 0) && (entry.onRead(entry.fd, entry.userArg)))
+			entry.activeEvents ^= peRead;
+		if (entry.activeEvents == 0)
+			this->haveEvent_.erase(it);
+	}
+
+	#if RASHEEQ_HAVE_EPOLL
+		::epoll_event events[R::Poller::maxPerPoll];
+		int res = ::epoll_wait(this->efd_, events, R::Poller::maxPerPoll, timeout);
+		if ((res == 0) || ((res == -1) && (errno == EINTR)))
+			return;
+		if (res == -1) {
+			//#TODO: Handle unexpected errors
+			return;
+		}
+		for (int i = 0; i < res; ++i) {
+			#if RASHEEQ_DEBUG_BUILD
+				//Same complexity as the release version, but with branching.
+				//None of these if conditions should evalutate as true, so these are
+				//really assertions for test purposes.
+				auto eit = this->entries_.find(events[i].data.fd);
+				if ((eit == this->entries_.end()) || (((events[i].events & EPOLLERR) != 0) && ((peError & entry.events) == 0))) {
+					//#TODO: Fatal here
+					continue;
+				}
+				Entry& entry = eit->second;
+			#else
+				Entry& entry = this->entries_[events[i].data.fd];
+			#endif /* RASHEEQ_DEBUG_BUILD */
+			int oactive = entry.activeEvents;
+			if (((events[i].events & EPOLLOUT) != 0) && (!entry.onWrite(entry.fd, entry.userArg)))
+				entry.activeEvents |= peWrite;
+			if (((events[i].events & EPOLLIN) != 0) && (!entry.onRead(entry.fd, entry.userArg)))
+				entry.activeEvents |= peRead;
+			if (entry.activeEvents != oactive)
+				this->haveEvent_.insert(entry.fd);
+		}
+	#else
+		//#TODO: kpoll/win32 api/fallback on select(), etc
+	#endif /* RASHEEQ_HAVE_EPOLL */
 };
 
 bool R::Poller::remove(int fd) {
 	auto it = this->entries_.find(fd);
 	if (it == this->entries_.end())
 		return false;
+	this->haveEvent_.erase(fd);
 	//#TODO: Support "close" event trigger
 	this->entries_.erase(it);
 	::close(fd);
@@ -74,19 +122,21 @@ bool R::Poller::add(int fd, ReadReady& onReadReady, WriteReady& onWriteReady, Er
 	if ((isMod) && (events == it->second.events))
 		return false;
 	#if RASHEEQ_HAVE_EPOLL
-	uint32_t ep_events = EPOLLET | EPOLLIN | EPOLLOUT;
-	if (onError != NULL)
-		ep_events |= EPOLLERR;
-	::epoll_event ep_event = {
-			events: ep_events,
-			data: { fd: fd }
-		};
-	//#TODO: Test for errors
-	::epoll_ctl(this->efd_, (isMod) ? EPOLL_CTL_MOD : EPOLL_CTL_ADD, fd, &ep_event);
+		uint32_t ep_events = EPOLLET | EPOLLIN | EPOLLOUT;
+		if (onError != NULL)
+			ep_events |= EPOLLERR;
+		::epoll_event ep_event = {
+				events: ep_events,
+				data: { fd: fd }
+			};
+		//#TODO: Test for errors
+		::epoll_ctl(this->efd_, (isMod) ? EPOLL_CTL_MOD : EPOLL_CTL_ADD, fd, &ep_event);
 	#endif /* RASHEEQ_HAVE_EPOLL */
 	Entry& entry = this->entries_[fd];
 	entry.fd = fd;
 	entry.events = events;
+	if (!isMod)
+		entry.activeEvents = 0;
 	entry.onRead = onReadReady;
 	entry.onWrite = onWriteReady;
 	entry.onError = (onError != NULL) ? *onError : ErrorOccurred();
@@ -116,7 +166,7 @@ void rasheeq_poller_poll(rasheeq_poller_t* poller) {
 	reinterpret_cast<R::Poller*>(poller)->poll();
 };
 
-void rasheeq_poller_timed_poll(rasheeq_poller_t* poller, const long timeout) {
+void rasheeq_poller_timed_poll(rasheeq_poller_t* poller, const int timeout) {
 	reinterpret_cast<R::Poller*>(poller)->poll(timeout);
 };
 
