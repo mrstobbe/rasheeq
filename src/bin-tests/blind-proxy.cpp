404063369c0d6b0b01d25347596a91d0b7bc89f9
mr.stobbe <mr.stobbe@gmail.com>
2012-08-10 05:46:47 +0000

    More work on a full blown TCPServer mechanism (added PollerPool and TCPClient)

--- src/bin-tests/blind-proxy.cpp
+++ src/bin-tests/blind-proxy.cpp
@@ -4,22 +4,45 @@
 #include <string>
 #include <iostream>
 
+#include <unordered_set>
+namespace R {
+
+class PollerPool {
+	protected:
+		std::unordered_set<Poller*> pollers_;
+	public:
+		PollerPool();
+		~PollerPool();
+	public:
+		Poller* add(int fd, Poller::ReadReady onReadReady, Poller::WriteReady onWriteReady);
+		Poller* add(int fd, Poller::ReadReady onReadReady, Poller::WriteReady onWriteReady, Poller::ErrorOccurred onError);
+		Poller* add(int fd, Poller::ReadReady onReadReady, Poller::WriteReady onWriteReady, void* userArg);
+		Poller* add(int fd, Poller::ReadReady onReadReady, Poller::WriteReady onWriteReady, Poller::ErrorOccurred onError, void* userArg);
+		Poller* createPoller();
+		bool remove(int fd);
+	private:
+		PollerPool(const PollerPool& copy) { };
+		PollerPool& operator =(const PollerPool& copy) { return *this; };
+};
+
+}; //ns R
+
 namespace R {
 
 class TCPServer {
 	protected:
 		int fd_;
 		bool greedy_;
-		Poller* poller_;
+		PollerPool* poller_;
 	public:
-		TCPServer(Poller* poller);
+		TCPServer(PollerPool* poller);
 		TCPServer(TCPServer&& move);
 		~TCPServer();
 		TCPServer& operator =(TCPServer&& move);
 	public:
 		void listen(const int port);
 		void listen(const bool ip6, const int port);
-		void listen(const std::string& ip, const int port);
+		void listen(const std::string& interface, const int port);
 		void listen(const std::string& local);
 	private:
 		void bind_(void* addr, const size_t addrSize);
@@ -33,11 +56,24 @@ class TCPServer {
 		static bool onWriteReady(int fd, void* arg);
 }; //class TCPServer
 
+class TCPClient {
+	protected:
+		TCPServer* server_;
+	protected:
+		TCPClient(TCPServer* server, int fd);
+	public:
+		TCPClient(TCPClient&& move);
+		~TCPClient();
+		TCPClient& operator =(TCPClient&& move);
+}; //class TCPClient
+
+
 }; //ns R
 
 int main(int argc, char** argv) {
-	R::Poller poller;
-	R::TCPServer server(&poller);
+	R::PollerPool pool;
+	pool->createPoller();
+	R::TCPServer server(&pool);
 };
 
 
@@ -48,7 +84,7 @@ int main(int argc, char** argv) {
 #include <fcntl.h>
 #include <unistd.h>
 
-R::TCPServer::TCPServer(R::Poller* poller):
+R::TCPServer::TCPServer(R::PollerPool* poller):
 	fd_(-1),
 	greedy_(false),
 	poller_(poller)
@@ -102,7 +138,7 @@ void R::TCPServer::listen(const bool ip6, const int port) {
 	this->listen_();
 };
 
-void R::TCPServer::listen(const std::string& ip, const int port) {
+void R::TCPServer::listen(const std::string& interface, const int port) {
 	#warning Not implemented
 };
 
@@ -112,7 +148,6 @@ void R::TCPServer::listen(const std::string& local) {
 
 void R::TCPServer::bind_(void* addr, const size_t addrSize) {
 	this->fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
-	#warning Obviously those lambdas one line down are just stubs
 	this->poller_->add(this->fd_, onReadReady, onWriteReady, this);
 	int v = ::fcntl(this->fd_, F_GETFL, 0);
 	::fcntl(this->fd_, F_SETFL, v | O_NONBLOCK);
@@ -142,17 +177,47 @@ R::TCPServer& R::TCPServer::operator =(TCPServer&& move) {
 
 bool R::TCPServer::onReadReady(int fd, void* arg) {
 	TCPServer* server = reinterpret_cast<TCPServer*>(arg);
-	int cli = ::accept(fd, NULL, NULL);
-	if (cli == -1)
-		return true;
-	//#TODO: Do something with the client
-	//#TODO: Support greedy (loop until would block)
-	#warning Stubbed. Obviously this should not close
-	::close(cli);
+	while (true) {
+		int cliFD = ::accept(fd, NULL, NULL);
+		if (cliFD == -1) {
+			//#TODO: Test for actual error (okay: EINTR, EAGAIN, or EWOULDBLOCK)
+			return true;
+		}
+		int v = ::fcntl(cliFD, F_GETFL, 0);
+		::fcntl(cliFD, F_SETFL, v | O_NONBLOCK);
+
+		if (!sever->greedy_)
+			break;
+	}
 	return false;
 };
 
 bool R::TCPServer::onWriteReady(int fd, void* arg) {
 	return true;
+};
+
+
+
+R::TCPServer::TCPClient(TCPServer* server, int fd):
+	fd_(fd),
+	server_(server)
+{
+};
+
+R::TCPServer::TCPClient(TCPClient&& move):
+	fd_(move.fd_),
+	server_(move.server_)
+{
+	move.fd_ = -1;
+	move.server_ = NULL;
+};
+
+R::TCPServer::~TCPClient() {
+	if (this->fd_ != -1) {
+	}
+};
+
+R::TCPClient& R::TCPServer::operator =(TCPClient&& move) {
+	return *this;
 };
 
